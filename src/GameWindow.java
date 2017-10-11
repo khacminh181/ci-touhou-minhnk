5a7c1ddf9319cc86953cebe971c2b47678703cc1
ThinkPad <nguyenkhacminh181@gmail.com>
2017-10-11 19:38:37 +0700

    Added game window

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -3,9 +3,14 @@ import java.awt.*;
 
 public class GameWindow extends JFrame{
 
+    GameCanvas canvas;
+
     public GameWindow() {
         this.setSize(800,600);
         this.setVisible(true);
-    }
+        this.setBackground(Color.BLACK);
 
+        this.canvas = new GameCanvas();
+        this.setContentPane(this.canvas);
+    }
 }
