be80205d2bdb6b7e68fac82be7b5c3d270fb1088
ThinkPad <nguyenkhacminh181@gmail.com>
2017-10-11 19:33:10 +0700

    Added game window

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,5 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("ádfasd");
-        System.out.println("Touhou is comming");
+        GameWindow gameWindow = new GameWindow();
     }
 }
